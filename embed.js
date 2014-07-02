d4209a31b72af7a592daedf5dd37f42f15a3ef49
Nathan Dinsmore <nfdins@gmail.com>
2014-07-02 12:13:17 -0400

    Add an option to hide controls/border in embedded player

--- embed.js
+++ embed.js
@@ -5,12 +5,21 @@
     return scripts[scripts.length - 1];
   })(document.getElementsByTagName('script'));
 
+  var hasUI = true;
+  var params = script.src.split('?')[1].split('&');
+  params.forEach(function(p) {
+    var parts = p.split('=');
+    if (parts.length > 1 && parts[0] === 'ui') {
+      hasUI = parts[1] !== 'false';
+    }
+  });
+
   var iframe = document.createElement('iframe');
   iframe.setAttribute('allowfullscreen', true);
   iframe.setAttribute('allowtransparency', true);
   iframe.src = script.src.replace(/embed\.js/, 'embed.html') + '&';
-  iframe.width = 482;
-  iframe.height = 393;
+  iframe.width = hasUI ? 482 : 480;
+  iframe.height = hasUI ? 393 : 360;
   iframe.style.border = '0';
   iframe.className = 'phosphorus';
 
