9c97169f13b1d59fd56ef7cfbad04c3112f20f0d
GarboMuffin <tommyweber33@gmail.com>
2019-09-08 12:20:04 -0500

    remove unused information from IO error logs

--- phosphorus.dist.js
+++ phosphorus.dist.js
@@ -2705,11 +2705,11 @@ var P;
                             reject(new Error(`HTTP Error ${xhr.status} while downloading ${this.url}`));
                         }
                     });
-                    xhr.addEventListener('error', () => {
-                        reject(`Error while downloading ${this.url} (onerror) (${xhr.status} ${xhr.statusText})`);
+                    xhr.addEventListener('error', (err) => {
+                        reject(`Error while downloading ${this.url} (error) (${xhr.status})`);
                     });
-                    xhr.addEventListener('abort', () => {
-                        reject(`Error while downloading ${this.url} (onabort) (${xhr.status} ${xhr.statusText})`);
+                    xhr.addEventListener('abort', (err) => {
+                        reject(`Error while downloading ${this.url} (abort) (${xhr.status})`);
                     });
                     xhr.open('GET', this.url);
                     xhr.responseType = this.type;
