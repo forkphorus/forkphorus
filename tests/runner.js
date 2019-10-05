3747da8316e0e181bfacba4f2501a42d9eabf4d9
--global <tommyweber33@gmail.com>
2019-10-05 14:20:46 -0500

    Improve logging of automated tests

--- tests/runner.js
+++ tests/runner.js
@@ -2,6 +2,8 @@ const puppeteer = require("puppeteer");
 const path = require('path');
 
 (async () => {
+  // If the tests take more than a minute, we can assume something went wrong and need to abort.
+  // In the future this might not be enough time, but for now it's much, much more than required.
   setTimeout(function() {
     console.error('[Runner] Test timed out');
     process.exit(1);
@@ -11,6 +13,7 @@ const path = require('path');
   const browser = await puppeteer.launch({
     headless: true,
     args: [
+      // Tests will be fetched from file:// URLs, which are normally too strict for this to work.
       '--allow-file-access-from-files',
     ],
   });
@@ -18,18 +21,28 @@ const path = require('path');
   console.log('[Runner] Opening new page');
   const page = await browser.newPage();
 
+  var currentTest = 'no active test';
   page.on('console', (msg) => {
-    console.log('[Log]', msg.text());
+    console.log('[Log]', '[' + currentTest + ']', msg.text());
   });
 
-  const file = path.join(__dirname, 'suite.html?automatedtest');
+  // We use some undocumented query string arguments to change the tests behavior a little bit.
+  // Notably we want to be absolutedly certain that the tests won't start running before we setup the environment
+  const file = path.join(__dirname, 'suite.html?automatedtest&nostart');
   console.log('[Runner] Opening file: ' + file);
   await page.goto(`file:${file}`);
+  await page.exposeFunction('startProjectHook', async (projectMeta) => {
+    const path = projectMeta.path;
+    currentTest = path;
+  });
 
   const results = await new Promise(async (resolve, reject) => {
     await page.exposeFunction('testsFinishedHook', async (results) => {
       await page.close();
       resolve(results.tests);
+    });
+    await page.evaluate(() => {
+      runTests();
     });
   });
 
