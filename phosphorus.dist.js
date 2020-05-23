daf7c2cdda056d6ffde01952f7905430fcc410f0
Thomas Weber <tommyweber33@gmail.com>
2020-05-22 20:57:05 -0500

    Update canvg to 3.0.6
    This seems to remove the perpetual frameloop from canvg, which should help with performance in some Scratch 2 projects.
    This also opens the door for actually scaling Scratch 2 SVGs.
    
    Build will probably fail because types are outdated. Will see if this is worthwhile before fixing those.

--- phosphorus.dist.js
+++ phosphorus.dist.js
@@ -4854,20 +4854,17 @@ var P;
                 patchSVG(svg, svg);
                 document.body.removeChild(svg);
                 svg.style.visibility = svg.style.position = svg.style.left = svg.style.top = '';
-                return new Promise((resolve, reject) => {
-                    const canvas = document.createElement('canvas');
-                    canvg(canvas, new XMLSerializer().serializeToString(svg), {
-                        ignoreMouse: true,
-                        ignoreAnimation: true,
-                        ignoreClear: true,
-                        renderCallback: function () {
-                            if (canvas.width === 0 || canvas.height === 0) {
-                                resolve(new Image());
-                                return;
-                            }
-                            resolve(canvas);
-                        }
-                    });
+                const canvas = document.createElement('canvas');
+                const ctx = canvas.getContext('2d');
+                return canvg.Canvg.from(ctx, new XMLSerializer().serializeToString(svg), {
+                    ignoreMouse: true,
+                    ignoreAnimation: true,
+                    ignoreClear: true,
+                })
+                    .then((v) => {
+                    return v.render();
+                }).then(() => {
+                    return canvas;
                 });
             }
             load() {
