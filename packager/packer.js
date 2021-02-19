4f177e624db2ed1452692d0ce4624d7dfdbc8ee1
Thomas Weber <weber_t@mailbox.org>
2021-02-19 15:27:58 -0600

    Zip support for packager

--- packager/packer.js
+++ packager/packer.js
@@ -332,7 +332,7 @@ window.Packer = (function() {
 <html>
   <head>
     <meta charset="utf-8">
-    <meta http-equiv="Content-Security-Policy" content="default-src 'unsafe-inline' 'unsafe-eval' data: blob:">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'unsafe-inline' 'unsafe-eval' 'self' data: blob:">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <style>
 /* Forkphorus styles... */
