7325db80b42c6075f5cfd16123603d5658679f7a
Thomas Weber <weber_t@mailbox.org>
2021-04-11 11:32:52 -0500

    Stop violating sulfurous' copyright

--- src/rendering/webgl.ts
+++ src/rendering/webgl.ts
@@ -2,6 +2,33 @@
 /// <reference path="renderer.ts" />
 /// <reference path="matrix.ts" />
 
+/*!
+Parts of the WebGL renderer are based on https://github.com/Mittagskogel/Sulfurous
+License:
+
+The MIT License (MIT)
+Copyright (c) 2013-2014 Nathan Dinsmore
+Copyright (c) 2016 Mittagskogel
+Copyright (c) 2017-2020 FRALEX
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 namespace P.renderer.webgl {
   import RotationStyle = P.core.RotationStyle;
 
