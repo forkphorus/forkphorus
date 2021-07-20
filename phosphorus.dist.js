7209110d4c137c8f76ea696533b28de28157baa0
Thomas Weber <weber_t@mailbox.org>
2021-07-19 23:56:23 -0500

    Update built JS files

--- phosphorus.dist.js
+++ phosphorus.dist.js
@@ -9566,8 +9566,8 @@ var P;
                 }
                 _reset(ctx, scale) {
                     const effectiveScale = scale * P.config.scale;
-                    const width = 480 * effectiveScale;
-                    const height = 360 * effectiveScale;
+                    const width = Math.max(1, 480 * effectiveScale);
+                    const height = Math.max(1, 360 * effectiveScale);
                     if (ctx.canvas.width !== width || ctx.canvas.height !== height) {
                         ctx.canvas.width = width;
                         ctx.canvas.height = height;
