8ce93c72cc8954a5d628dfe264072a1eaff26cbd
Thomas Weber <muffin@muffin.ink>
2022-06-01 19:47:52 -0500

    For now, don't actually use project tokens even though we're fetching them

--- phosphorus.dist.js
+++ phosphorus.dist.js
@@ -3579,9 +3579,6 @@ var P;
             }
             fetchProject(id, token) {
                 let url = this.options.projectHost.replace('$id', id);
-                if (token) {
-                    url += `?token=${token}`;
-                }
                 const request = new P.io.Request(url);
                 return request
                     .ignoreErrors()
--- src/player.ts
+++ src/player.ts
@@ -999,9 +999,6 @@ namespace P.player {
      */
     private fetchProject(id: string, token: string | null): Promise<Blob> {
       let url = this.options.projectHost.replace('$id', id);
-      if (token) {
-        url += `?token=${token}`;
-      }
       const request = new P.io.Request(url);
       return request
         .ignoreErrors()
