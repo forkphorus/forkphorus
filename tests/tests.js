71f1003bffbcd7f57e788719cf00a42229a0c6b4
GarboMuffin <muffin@mailbox.org>
2022-08-27 17:00:29 -0500

    Add test for zero second waits, rests, drums in warp mode

--- tests/tests.js
+++ tests/tests.js
@@ -41,6 +41,7 @@
     'sb3/boolean-number-comparison.sb3',
     'sb3/non-finite-size-100-brightness.sb3',
     'sb3/random-with-invalid-number-with-period.sb3',
+    'sb3/wait-zero-seconds-in-warp-mode.sb3',
 
     'scratch-vm/broadcast-wait-arg-change.sb2',
     'scratch-vm/control-if-false-then-else.sb2',
