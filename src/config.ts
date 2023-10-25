b5cd46e6c911b3867adfb1ca7a7779da33c823c0
GarboMuffin <muffin@mailbox.org>
2023-10-24 20:11:46 -0500

    Only use vector rasterizing in Firefox
    It was already disabled in Safari. This disables it in Chromium browsers too.
    In almost all cases, this is faster, looks better, and uses less memory.
    Only exception is Firefox, where drawing SVGs is extremely slow for some reason.
    
    Related discussion: https://github.com/forkphorus/forkphorus/issues/654

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,10 @@ namespace P.config {
   export var useWebGL = false;
   export var supportVideoSensing = false;
   export var experimentalOptimizations = false;
-  export var allowRasterizeVectors = true;
   export var scale = window.devicePixelRatio || 1;
   export var PROJECT_API: string = 'https://projects.scratch.mit.edu/$id';
+
+  // Firefox's drawImage() performance with SVGs is too slow to be viable, but in other
+  // browsers, skipping rasterization usually is faster, looks better, and uses less memory.
+  export var allowRasterizeVectors = navigator.userAgent.includes('Firefox');
 }
