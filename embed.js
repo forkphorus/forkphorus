262f86ed4e6fbdff944aa036e82643312fc0f600
Muffin <muffin@muffin.ink>
2024-09-24 20:20:57 -0500

    SECURITY: Fix DOM clobbering vulnerability in deprecated embed.js
    Note that embed.js has been deprecated since May 2020 in favor of embedding
    embed.html directly, so this affects very few people. embed.html is immune from
    the issues described below.
    
    If the page embed.js is used by **already contains a security bug** that allows
    attackers to insert <img src=malicious name=currentScript> into the page, then
    embed.js would allow elevating that to creating an <iframe> with src controlled
    by the attacker.
    
    This bug dates back to when embeds were added to phosphorus in 2014.
    
    While we're here:
    - remove legacy document.currentScript fallback; all browsers we care about
      support it already and the fallback could be vulnerable to more bugs
    - make iframe src generation more resilient to attacks or weird states by only
      replacing embed.js specifically in the path, not eg. the hostname.

--- embed.js
+++ embed.js
@@ -4,16 +4,19 @@
 (function() {
   'use strict';
 
-  // compatibility: embed.js does not work in some common WYSIWYG editors
-  // security: this script could potentially do things like steal browser cookies. an <iframe> should not be able to do this ("same origin policy")
-  // embed.js won't be removed soon but it may be removed "eventually" and is not actively tested
+  // Compatibility: The embed.js <script> tag does not work in some common WYSIWYG website designers.
+  // Security: The <script> tag full access to the website it is embedded in while an <iframe> is sandboxed. There has also been some known security vulnerabilities that the iframe is immune from.
   console.warn('forkphorus: embed.js is deprecated due to compatibility and security concerns and may stop working in the future. Please use a direct <iframe> of embed.html instead! The generator on https://forkphorus.github.io/ has been updated to do this.');
 
   // Get this script's HTML element.
   // We'll replace the script element with the frame.
-  var script = document.currentScript || (function(scripts) {
-    return scripts[scripts.length - 1];
-  })(document.getElementsByTagName('script'));
+  var script = document.currentScript;
+
+  // Named elements can override document.currentScript. Because we generate the iframe's source from the
+  // script's source, blindly using the source from that named element would be a security bug.
+  if (script.tagName.toUpperCase() !== 'SCRIPT') {
+    throw new Error('forkphorus: document.currentScript is not a script. This indicates a DOM clobbering attack. Refusing to continue for security.');
+  }
 
   // Determine the hasUI option to properly size the frame.
   var hasUI = true;
@@ -38,7 +41,15 @@
   var iframe = document.createElement('iframe');
   iframe.setAttribute('allowfullscreen', true);
   iframe.setAttribute('allowtransparency', true);
-  iframe.src = script.src.replace('embed.js', 'embed.html');
+
+  // Rewrite embed.js to embed.html
+  var iframeSrc = new URL(script.src);
+  if (!iframeSrc.pathname.endsWith('/embed.js')) {
+    throw new Error('forkphorus: embed script is not named embed.js. Unable to safely generate <iframe> src. Refusing to continue for security.');
+  }
+  iframeSrc.pathname = iframeSrc.pathname.replace(/\/embed\.js$/, '/embed.html');
+  iframe.src = iframeSrc.href;
+
   if (hasUI) {
     // include enough for controls and the player border
     iframe.width = baseWidth + 2;
